317bc6882dd985bc7170d35c3cfe827555eaa7c4
PierreDV <p.verteuil@gmail.com>
2019-03-14 13:02:45 +0100

    add tests to ensure text area is reacting to user events properly

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
 let wrapped;
 
 beforeEach(() => {
-  wrapped = mount(<CommentBox />);
-});
-
-afterEach(() => {
-  wrapped.unmount();
+  wrapped = shallow(<CommentBox />);
 });
 
 it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', { 
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  });
+
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+  
+  it('empties the text area when form is submitted', () => {
+    wrapped.find('form').simulate('submit', {
+      preventDefault: () => {}
+    });
+    wrapped.update();
+  
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
